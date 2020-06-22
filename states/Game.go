33081af6adbf4f141e3cf9d246082b978710e0a8
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-21 17:14:30 -0700

    Add GameContainer el and send test movement Cmds

--- states/Game.go
+++ states/Game.go
@@ -11,6 +11,7 @@ import (
 // and joined as a player character.
 type Game struct {
 	client.State
+	GameContainer   ui.Container
 	ChatWindow      ui.Container
 	MapWindow       ui.Container
 	InventoryWindow ui.Container
@@ -26,6 +27,45 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 	s.world.Init(s.Client)
 
 	s.Client.Log.Print("Game State")
+
+	// Main Container
+	err = s.GameContainer.Setup(ui.ContainerConfig{
+		Value: "Game",
+		Style: `
+			W 100%
+			H 100%
+			BackgroundColor 139 186 139 255
+		`,
+		Events: ui.Events{
+			OnKeyDown: func(char uint8, modifiers uint16) bool {
+				// TODO: Forward this to a full bind/keypress handler system.
+				if char == 107 || char == 82 { // up
+					s.Client.Log.Println("send north")
+					s.Client.Send(network.CommandCmd{
+						Cmd: network.North,
+					})
+				} else if char == 106 || char == 81 { // down
+					s.Client.Log.Println("send south")
+					s.Client.Send(network.CommandCmd{
+						Cmd: network.South,
+					})
+				} else if char == 104 || char == 80 { // left
+					s.Client.Log.Println("send west")
+					s.Client.Send(network.CommandCmd{
+						Cmd: network.West,
+					})
+				} else if char == 108 || char == 79 { // right
+					s.Client.Log.Println("send east")
+					s.Client.Send(network.CommandCmd{
+						Cmd: network.East,
+					})
+				}
+				return true
+			},
+		},
+	})
+	s.Client.RootWindow.AdoptChannel <- s.GameContainer.This
+
 	// Sub-window: map
 	err = s.MapWindow.Setup(ui.ContainerConfig{
 		Value: "Map",
@@ -42,7 +82,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 		Value: "Map",
 	})
 	s.MapWindow.AdoptChannel <- mapText
-	s.Client.RootWindow.AdoptChannel <- s.MapWindow.This
+	s.GameContainer.AdoptChannel <- s.MapWindow.This
 	// Sub-window: chat
 	err = s.ChatWindow.Setup(ui.ContainerConfig{
 		Value: "Chat",
@@ -54,7 +94,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 			BackgroundColor 0 0 128 128
 		`,
 	})
-	s.Client.RootWindow.AdoptChannel <- s.ChatWindow.This
+	s.GameContainer.AdoptChannel <- s.ChatWindow.This
 	// Sub-window: inventory
 	err = s.InventoryWindow.Setup(ui.ContainerConfig{
 		Value: "Inventory",
@@ -67,7 +107,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 			BackgroundColor 0 128 0 128
 		`,
 	})
-	s.Client.RootWindow.AdoptChannel <- s.InventoryWindow.This
+	s.GameContainer.AdoptChannel <- s.InventoryWindow.This
 	s.InventoryWindow.SetHidden(true)
 	// Sub-window: ground
 	err = s.GroundWindow.Setup(ui.ContainerConfig{
@@ -79,7 +119,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 			BackgroundColor 128 128 128 128
 		`,
 	})
-	s.Client.RootWindow.AdoptChannel <- s.GroundWindow.This
+	s.GameContainer.AdoptChannel <- s.GroundWindow.This
 	// Sub-window: stats
 	err = s.StatsWindow.Setup(ui.ContainerConfig{
 		Value: "Stats",
@@ -90,7 +130,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 			BackgroundColor 128 0 0 128
 		`,
 	})
-	s.Client.RootWindow.AdoptChannel <- s.StatsWindow.This
+	s.GameContainer.AdoptChannel <- s.StatsWindow.This
 	s.StatsWindow.SetHidden(true)
 	// Sub-window: state
 	err = s.StateWindow.Setup(ui.ContainerConfig{
@@ -103,7 +143,7 @@ func (s *Game) Init(t interface{}) (state client.StateI, nextArgs interface{}, e
 			BackgroundColor 128 128 0 128
 		`,
 	})
-	s.Client.RootWindow.AdoptChannel <- s.StateWindow.This
+	s.GameContainer.AdoptChannel <- s.StateWindow.This
 	s.StateWindow.SetHidden(true)
 	//
 	//go s.Client.LoopCmd()
