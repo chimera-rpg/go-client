65268d2c34644ba518b01f976205b8266012e093
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-25 22:15:06 -0700

    Add X,Y,Z, and Gone fields

--- world/Object.go
+++ world/Object.go
@@ -5,4 +5,6 @@ type Object struct {
 	Type        uint8
 	AnimationID uint32
 	FaceID      uint32
+	Y, X, Z     uint32 // We keep Y, X, Z information here to make it easier to render objects. This is updated when Tile updates are received.
+	Gone        bool   // Represents if the object is currently in an unknown location. This happens when a Tile that holds an Object no longer holds it and no other Tile has claimed it.
 }
