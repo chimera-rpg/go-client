735443a7a2ecb62f1d65314d14e4f1cb9470c639
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-27 07:35:57 -0700

    Add UpdateX and UpdateY

--- ui/Update.go
+++ ui/Update.go
@@ -8,3 +8,16 @@ type UpdateI interface {
 type UpdateValue struct {
 	Value string
 }
+
+// UpdateX represents a change to the X of an Element.
+type UpdateX struct {
+	Number
+}
+
+// UpdateY represents a change to the X of an Element.
+type UpdateY struct {
+	Number
+}
+
+// UpdateStyle represents an update to the Style of an element.
+type UpdateStyle = string
