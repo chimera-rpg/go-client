ac4b9f6b2ec9ddca2fba62f89b69b2947dd68bf9
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-28 21:15:00 -0700

    Add Object Index field

--- world/Object.go
+++ world/Object.go
@@ -5,6 +5,7 @@ type Object struct {
 	Type        uint8
 	AnimationID uint32
 	FaceID      uint32
+	Index       int    // Position in its owning Tile.
 	Y, X, Z     uint32 // We keep Y, X, Z information here to make it easier to render objects. This is updated when Tile updates are received.
 	H, W, D     uint8
 	Missing     bool // Represents if the object is currently in an unknown location. This happens when a Tile that holds an Object no longer holds it and no other Tile has claimed it.
