062121d8fa0fa94cd5dcb78201f8404965061b9d
kts of kettek <kts@kettek.net>
2021-10-01 00:17:06 -0700

    Use variables for default keybinds

--- states/Game_Binds.go
+++ states/Game_Binds.go
@@ -6,6 +6,85 @@ import (
 	"github.com/chimera-rpg/go-common/network"
 )
 
+var (
+	defaultNorth1 = binds.KeyGroup{
+		Keys:    []uint8{107},
+		Pressed: true,
+	}
+	defaultNorth2 = binds.KeyGroup{
+		Keys:    []uint8{82},
+		Pressed: true,
+	}
+	defaultNorthRun1 = binds.KeyGroup{
+		Keys:    []uint8{107},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultNorthRun2 = binds.KeyGroup{
+		Keys:    []uint8{82},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultSouth1 = binds.KeyGroup{
+		Keys:    []uint8{106},
+		Pressed: true,
+	}
+	defaultSouth2 = binds.KeyGroup{
+		Keys:    []uint8{81},
+		Pressed: true,
+	}
+	defaultSouthRun1 = binds.KeyGroup{
+		Keys:    []uint8{106},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultSouthRun2 = binds.KeyGroup{
+		Keys:    []uint8{81},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultWest1 = binds.KeyGroup{
+		Keys:    []uint8{104},
+		Pressed: true,
+	}
+	defaultWest2 = binds.KeyGroup{
+		Keys:    []uint8{80},
+		Pressed: true,
+	}
+	defaultWestRun1 = binds.KeyGroup{
+		Keys:    []uint8{104},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultWestRun2 = binds.KeyGroup{
+		Keys:    []uint8{80},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultEast1 = binds.KeyGroup{
+		Keys:    []uint8{108},
+		Pressed: true,
+	}
+	defaultEast2 = binds.KeyGroup{
+		Keys:    []uint8{79},
+		Pressed: true,
+	}
+	defaultEastRun1 = binds.KeyGroup{
+		Keys:    []uint8{108},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultEastRun2 = binds.KeyGroup{
+		Keys:    []uint8{79},
+		Pressed: true,
+		Repeat:  true,
+	}
+	defaultFocusChat = binds.KeyGroup{
+		Keys:    []uint8{13},
+		Pressed: true,
+	}
+)
+
 func (s *Game) SetupBinds() {
 	s.bindings = &s.Client.DataManager.Config.Game.Bindings
 	s.bindings.Init()
@@ -54,98 +133,39 @@ func (s *Game) SetupBinds() {
 		s.ChatInput.GetUpdateChannel() <- ui.UpdateFocus{}
 	})
 	if !s.bindings.HasKeygroupsForName("north") {
-		s.bindings.AddKeygroup("north", binds.KeyGroup{
-			Keys:    []uint8{107},
-			Pressed: true,
-		})
-		s.bindings.AddKeygroup("north", binds.KeyGroup{
-			Keys:    []uint8{82},
-			Pressed: true,
-		})
+		s.bindings.AddKeygroup("north", defaultNorth1)
+		s.bindings.AddKeygroup("north", defaultNorth2)
 	}
 	if !s.bindings.HasKeygroupsForName("north run") {
-		s.bindings.AddKeygroup("north run", binds.KeyGroup{
-			Keys:    []uint8{107},
-			Pressed: true,
-			Repeat:  true,
-		})
-		s.bindings.AddKeygroup("north run", binds.KeyGroup{
-			Keys:    []uint8{82},
-			Pressed: true,
-			Repeat:  true,
-		})
+		s.bindings.AddKeygroup("north run", defaultNorthRun1)
+		s.bindings.AddKeygroup("north run", defaultNorthRun2)
 	}
 	if !s.bindings.HasKeygroupsForName("south") {
-		s.bindings.AddKeygroup("south", binds.KeyGroup{
-			Keys:    []uint8{106},
-			Pressed: true,
-		})
-		s.bindings.AddKeygroup("south", binds.KeyGroup{
-			Keys:    []uint8{81},
-			Pressed: true,
-		})
+		s.bindings.AddKeygroup("south", defaultSouth1)
+		s.bindings.AddKeygroup("south", defaultSouth2)
 	}
 	if !s.bindings.HasKeygroupsForName("south run") {
-		s.bindings.AddKeygroup("south run", binds.KeyGroup{
-			Keys:    []uint8{106},
-			Pressed: true,
-			Repeat:  true,
-		})
-		s.bindings.AddKeygroup("south run", binds.KeyGroup{
-			Keys:    []uint8{81},
-			Pressed: true,
-			Repeat:  true,
-		})
+		s.bindings.AddKeygroup("south run", defaultSouthRun1)
+		s.bindings.AddKeygroup("south run", defaultSouthRun2)
 	}
 	if !s.bindings.HasKeygroupsForName("west") {
-		s.bindings.AddKeygroup("west", binds.KeyGroup{
-			Keys:    []uint8{104},
-			Pressed: true,
-		})
-		s.bindings.AddKeygroup("west", binds.KeyGroup{
-			Keys:    []uint8{80},
-			Pressed: true,
-		})
+		s.bindings.AddKeygroup("west", defaultWest1)
+		s.bindings.AddKeygroup("west", defaultWest2)
 	}
 	if !s.bindings.HasKeygroupsForName("west run") {
-		s.bindings.AddKeygroup("west run", binds.KeyGroup{
-			Keys:    []uint8{104},
-			Pressed: true,
-			Repeat:  true,
-		})
-		s.bindings.AddKeygroup("west run", binds.KeyGroup{
-			Keys:    []uint8{80},
-			Pressed: true,
-			Repeat:  true,
-		})
+		s.bindings.AddKeygroup("west run", defaultWestRun1)
+		s.bindings.AddKeygroup("west run", defaultWestRun2)
 	}
 	if !s.bindings.HasKeygroupsForName("east") {
-		s.bindings.AddKeygroup("east", binds.KeyGroup{
-			Keys:    []uint8{108},
-			Pressed: true,
-		})
-		s.bindings.AddKeygroup("east", binds.KeyGroup{
-			Keys:    []uint8{79},
-			Pressed: true,
-		})
+		s.bindings.AddKeygroup("east", defaultEast1)
+		s.bindings.AddKeygroup("east", defaultEast2)
 	}
 	if !s.bindings.HasKeygroupsForName("east run") {
-		s.bindings.AddKeygroup("east run", binds.KeyGroup{
-			Keys:    []uint8{108},
-			Pressed: true,
-			Repeat:  true,
-		})
-		s.bindings.AddKeygroup("east run", binds.KeyGroup{
-			Keys:    []uint8{79},
-			Pressed: true,
-			Repeat:  true,
-		})
+		s.bindings.AddKeygroup("east run", defaultEastRun1)
+		s.bindings.AddKeygroup("east run", defaultEastRun2)
 	}
 
 	if !s.bindings.HasKeygroupsForName("focus chat") {
-		s.bindings.AddKeygroup("focus chat", binds.KeyGroup{
-			Keys:    []uint8{13},
-			Pressed: true,
-		})
+		s.bindings.AddKeygroup("focus chat", defaultFocusChat)
 	}
 }
