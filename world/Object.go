68b9e85fd4a6bc11610a4b47bfd8453d6251cb01
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-10-04 18:36:19 -0700

    Add Squeezing and Crouching fields

--- world/Object.go
+++ world/Object.go
@@ -11,6 +11,8 @@ type Object struct {
 	H, W, D     uint8
 	Missing     bool // Represents if the object is currently in an unknown location. This happens when a Tile that holds an Object no longer holds it and no other Tile has claimed it.
 	Changed     bool // Represents if the object's position has been changed. Cleared by Game.RenderObject
+	Squeezing   bool // Represents if the object is squeezing. Causes the rendered image to be lightly squashed in the X axis.
+	Crouching   bool // Represents if the object is crouching. Causes the rendered image to be lightly squashed in the Y axis.
 }
 
 // ObjectsFilter returns a new slice containing all Objects in the slice that satisfy the predicate f.
