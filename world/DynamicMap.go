a6eb834009b29cf84e34a50db9f205af2f44e827
kts of kettek <kts@kettek.net>
2022-01-25 07:22:02 -0800

    Remove unneeded camera code

--- world/DynamicMap.go
+++ world/DynamicMap.go
@@ -4,12 +4,6 @@ package world
 type DynamicMap struct {
 	tiles                [][][]DynamicMapTile
 	height, width, depth uint32
-	cameraX              uint32
-	cameraY              uint32
-	cameraZ              uint32
-	cameraW              uint32 // Width of the camera's viewport
-	cameraD              uint32 // Depth of the camera's viewport
-	cameraH              uint32 // Height of the camera's viewport
 }
 
 // Init initializes the DynamicMap.
@@ -30,28 +24,6 @@ func (d *DynamicMap) SetTile(y, x, z uint32, objectIDs []uint32) {
 		return
 	}
 	d.tiles[y][x][z] = DynamicMapTile{objectIDs}
-}
-
-// SetCamera sets the current camera position to y, x, z
-func (d *DynamicMap) SetCamera(y, x, z uint32) {
-	d.cameraY = y
-	d.cameraX = x
-	d.cameraZ = z
-}
-
-// GetCameraView gets a 3D slice of the current camera view.
-func (d *DynamicMap) GetCameraView() [][][]DynamicMapTile {
-	tile := make([][][]DynamicMapTile, d.cameraW)
-	for x := -d.cameraW / 2; x < d.cameraW/2; x++ {
-		tile[d.cameraX+x] = make([][]DynamicMapTile, d.cameraD)
-		for y := -d.cameraD / 2; y < d.cameraD/2; y++ {
-			tile[d.cameraX+x][d.cameraY+y] = make([]DynamicMapTile, d.cameraH)
-			for z := -d.cameraH / 2; z < d.cameraH/2; z++ {
-				tile[d.cameraX+x][d.cameraY+y][d.cameraZ+z] = d.GetTile(int(y), int(x), int(z))
-			}
-		}
-	}
-	return tile
 }
 
 // GetTile gets the tile stack at Y, X, Z.
