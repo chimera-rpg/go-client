c56b23a4089c59471d536130ad63c0a12f773f25
kts of kettek <kts@kettek.net>
2022-01-25 19:24:12 -0800

    Add PrimitiveElement

---
+++ ui/PrimitiveElement-SDL.go
@@ -0,0 +1,80 @@
+//go:build !mobile
+// +build !mobile
+
+package ui
+
+import (
+	"github.com/veandco/go-sdl2/gfx"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+// PrimitiveElement is the element responsible for rendering a primitive.
+type PrimitiveElement struct {
+	BaseElement
+	Shape PrimitiveShape
+}
+
+func (p *PrimitiveElement) Render() {
+	if p.IsHidden() {
+		return
+	}
+	if p.Shape == RectangleShape {
+		// Draw filled box
+		dst := sdl.Rect{
+			X: p.x,
+			Y: p.y,
+			W: p.w,
+			H: p.h,
+		}
+		if p.Style.BackgroundColor.A > 0 {
+			p.Context.Renderer.SetDrawColor(
+				p.Style.BackgroundColor.R,
+				p.Style.BackgroundColor.G,
+				p.Style.BackgroundColor.B,
+				p.Style.BackgroundColor.A,
+			)
+			p.Context.Renderer.FillRect(&dst)
+		}
+		// Draw outline
+		if p.Style.OutlineColor.A > 0 {
+			p.Context.Renderer.SetDrawColor(
+				p.Style.OutlineColor.R,
+				p.Style.OutlineColor.G,
+				p.Style.OutlineColor.B,
+				p.Style.OutlineColor.A,
+			)
+			p.Context.Renderer.DrawRect(&dst)
+
+		}
+	} else if p.Shape == EllipseShape {
+		// Draw filled shape.
+		if p.Style.BackgroundColor.A > 0 {
+			gfx.FilledEllipseRGBA(
+				p.Context.Renderer,
+				p.x,
+				p.y,
+				p.w/2,
+				p.h/2,
+				p.Style.BackgroundColor.R,
+				p.Style.BackgroundColor.G,
+				p.Style.BackgroundColor.B,
+				p.Style.BackgroundColor.A,
+			)
+		}
+		// Draw outline.
+		if p.Style.OutlineColor.A > 0 {
+			gfx.EllipseRGBA(
+				p.Context.Renderer,
+				p.x,
+				p.y,
+				p.w/2,
+				p.h/2,
+				p.Style.OutlineColor.R,
+				p.Style.OutlineColor.G,
+				p.Style.OutlineColor.B,
+				p.Style.OutlineColor.A,
+			)
+		}
+	}
+	p.BaseElement.Render()
+}
---
+++ ui/PrimitiveElement.go
@@ -0,0 +1,33 @@
+package ui
+
+// PrimitiveShape represents our enum for shapes.
+type PrimitiveShape int
+
+//
+const (
+	RectangleShape PrimitiveShape = iota
+	EllipseShape
+)
+
+// PrimitiveElementConfgi is the configuration for construction.
+type PrimitiveElementConfig struct {
+	Style  string
+	Shape  PrimitiveShape
+	Events Events
+}
+
+// PrimitiveElementStyle is our default style for PrimitiveElement.
+var PrimitiveElementStyle = `
+`
+
+// NewPrimitiveElement creates a new PrimitiveEllemnt from the passed configuration.
+func NewPrimitiveElement(c PrimitiveElementConfig) ElementI {
+	p := PrimitiveElement{}
+	p.This = ElementI(&p)
+	p.Style.Alpha.Set(1)
+	p.Style.Parse(PrimitiveElementStyle)
+	p.Style.Parse(c.Style)
+	p.Shape = c.Shape
+	p.Events = c.Events
+	return ElementI(&p)
+}
